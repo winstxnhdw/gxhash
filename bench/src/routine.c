dc235515299a1612f64bdf7edc3251a25f2f7aa6
winstxnhdw <winstxnhdw@gmail.com>
2026-03-22 00:01:51 +0800

    internal(bench/perf): optimise every single instruction

--- bench/src/routine.c
+++ bench/src/routine.c
@@ -10,19 +10,51 @@ static struct PyModuleDef module_definition = {
 
 typedef struct {
   PyObject ob_base;
-  PyObject *callback;
-  PyObject *stack;
-  PyObject *kwarg_names;
   PyObject *result;
+  PyObject *callback;
+  PyObject *kwarg_names;
+  PyObject **items;
+  vectorcallfunc self_vector_call;
+  vectorcallfunc vector_call;
 } EagerRoutineObject;
 
 static void EagerRoutine_dealloc(PyObject *const self_obj) {
   EagerRoutineObject *const self = (EagerRoutineObject *)self_obj;
+
+  if (self->items != NULL) {
+    for (Py_ssize_t i = 0; i < 1 + (self->kwarg_names ? PyTuple_GET_SIZE(self->kwarg_names) : 0); i++) {
+      Py_XDECREF(self->items[i]);
+    }
+
+    PyMem_Free(self->items);
+  }
+
   Py_XDECREF(self->callback);
-  Py_XDECREF(self->stack);
   Py_XDECREF(self->kwarg_names);
   Py_XDECREF(self->result);
   Py_TYPE(self)->tp_free(self);
+}
+
+static PyObject *EagerRoutine_vectorcall(
+    PyObject *const self_obj,
+    PyObject *const *args,
+    const size_t args_flag,
+    PyObject *const kwargs
+) {
+  EagerRoutineObject *const self = (EagerRoutineObject *)self_obj;
+  self->items[0] = args[0];
+  self->result = self->vector_call(self->callback, self->items, 1, self->kwarg_names);
+  self->items[0] = Py_None;
+
+  return Py_NewRef(self_obj);
+}
+
+static PySendResult EagerRoutine_am_send(PyObject *const self_obj, PyObject *const arg, PyObject **presult) {
+  EagerRoutineObject *const self = (EagerRoutineObject *)self_obj;
+  *presult = self->result;
+  self->result = Py_None;
+
+  return PYGEN_RETURN;
 }
 
 static PyObject *EagerRoutine_new(PyTypeObject *type, PyObject *args, PyObject *kwargs) {
@@ -41,56 +73,33 @@ static PyObject *EagerRoutine_new(PyTypeObject *type, PyObject *args, PyObject *
     goto error;
   }
 
-  if ((self->stack = PyTuple_New(kwargs_count + 1)) == NULL) {
+  if ((self->items = PyMem_Calloc(kwargs_count + 1, sizeof(PyObject *))) == NULL) {
+    PyErr_NoMemory();
     goto error;
   }
 
-  if ((self->kwarg_names = PyTuple_New(kwargs_count)) == NULL) {
+  if (kwargs_count > 0 && (self->kwarg_names = PyTuple_New(kwargs_count)) == NULL) {
     goto error;
   }
 
   self->callback = Py_NewRef(PyTuple_GET_ITEM(args, 0));
+  self->self_vector_call = EagerRoutine_vectorcall;
   self->result = Py_None;
+
+  const vectorcallfunc vector_call_function = PyVectorcall_Function(self->callback);
+  self->vector_call = vector_call_function ? vector_call_function : PyObject_Vectorcall;
+  self->items[0] = Py_None;
 
   for (Py_ssize_t i = 0; i < kwargs_count && PyDict_Next(kwargs, &position, &key, &value); i++) {
     PyTuple_SET_ITEM(self->kwarg_names, i, Py_NewRef(key));
-    PyTuple_SET_ITEM(self->stack, 1 + i, Py_NewRef(value));
+    self->items[1 + i] = Py_NewRef(value);
   }
 
-  PyTuple_SET_ITEM(self->stack, 0, Py_NewRef(Py_None));
   return (PyObject *)self;
 
 error:
   Py_XDECREF(self);
   return NULL;
-}
-
-static PyObject *EagerRoutine_call(PyObject *const self_obj, PyObject *args, PyObject *kwds) {
-  PyObject *data = NULL;
-
-  if (!PyArg_ParseTuple(args, "O:__call__", &data)) {
-    return NULL;
-  }
-
-  EagerRoutineObject *const self = (EagerRoutineObject *)self_obj;
-  PyObject **const items = &PyTuple_GET_ITEM(self->stack, 0);
-  items[0] = data;
-
-  if ((self->result = PyObject_Vectorcall(self->callback, items, 1, self->kwarg_names)) == NULL) {
-    items[0] = Py_None;
-    return NULL;
-  }
-
-  items[0] = Py_None;
-  return Py_NewRef(self_obj);
-}
-
-static PySendResult EagerRoutine_am_send(PyObject *const self_obj, PyObject *Py_UNUSED(arg), PyObject **presult) {
-  EagerRoutineObject *const self = (EagerRoutineObject *)self_obj;
-  *presult = self->result;
-  self->result = Py_None;
-
-  return PYGEN_RETURN;
 }
 
 static PyAsyncMethods EagerRoutine_async = {
@@ -99,12 +108,13 @@ static PyAsyncMethods EagerRoutine_async = {
 
 static PyTypeObject EagerRoutine_Type = {
     .ob_base = {PyObject_HEAD_INIT(NULL) 0},
-    .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_IMMUTABLETYPE,
+    .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_IMMUTABLETYPE | Py_TPFLAGS_HAVE_VECTORCALL,
     .tp_name = "routine.EagerRoutine",
+    .tp_vectorcall_offset = __builtin_offsetof(EagerRoutineObject, self_vector_call),
     .tp_basicsize = sizeof(EagerRoutineObject),
+    .tp_call = PyVectorcall_Call,
     .tp_new = EagerRoutine_new,
     .tp_dealloc = EagerRoutine_dealloc,
-    .tp_call = EagerRoutine_call,
     .tp_as_async = &EagerRoutine_async,
 };
 
