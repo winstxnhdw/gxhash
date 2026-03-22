6ff7143861b6251dfb84e06d2a85c0911a27fee3
winstxnhdw <winstxnhdw@gmail.com>
2026-03-22 14:27:15 +0800

    style(clang): set indent width to 4

--- bench/src/routine.c
+++ bench/src/routine.c
@@ -5,13 +5,13 @@
 typedef struct EagerRoutineObject EagerRoutineObject;
 
 struct EagerRoutineObject {
-  PyObject ob_base;
-  PyObject *result;
-  PyObject *callback;
-  PyObject *kwarg_names;
-  PyObject **items;
-  vectorcallfunc self_vector_call;
-  vectorcallfunc vector_call;
+    PyObject ob_base;
+    PyObject *result;
+    PyObject *callback;
+    PyObject *kwarg_names;
+    PyObject **items;
+    vectorcallfunc self_vector_call;
+    vectorcallfunc vector_call;
 };
 
 static struct PyModuleDef module_definition = {
@@ -21,20 +21,20 @@ static struct PyModuleDef module_definition = {
 };
 
 static void EagerRoutine_dealloc(PyObject *const self_obj) {
-  EagerRoutineObject *const self = (EagerRoutineObject *)self_obj;
+    EagerRoutineObject *const self = (EagerRoutineObject *)self_obj;
 
-  if (self->items != NULL) {
-    for (Py_ssize_t i = 0; i < 1 + (self->kwarg_names ? PyTuple_GET_SIZE(self->kwarg_names) : 0); i++) {
-      Py_XDECREF(self->items[i]);
+    if (self->items != NULL) {
+        for (Py_ssize_t i = 0; i < 1 + (self->kwarg_names ? PyTuple_GET_SIZE(self->kwarg_names) : 0); i++) {
+            Py_XDECREF(self->items[i]);
+        }
+
+        PyMem_Free(self->items);
     }
 
-    PyMem_Free(self->items);
-  }
-
-  Py_XDECREF(self->callback);
-  Py_XDECREF(self->kwarg_names);
-  Py_XDECREF(self->result);
-  Py_TYPE(self)->tp_free(self);
+    Py_XDECREF(self->callback);
+    Py_XDECREF(self->kwarg_names);
+    Py_XDECREF(self->result);
+    Py_TYPE(self)->tp_free(self);
 }
 
 static PyObject *EagerRoutine_vectorcall(
@@ -43,63 +43,63 @@ static PyObject *EagerRoutine_vectorcall(
     const size_t args_flag,
     PyObject *const kwargs
 ) {
-  EagerRoutineObject *const self = (EagerRoutineObject *)self_obj;
-  self->items[0] = args[0];
-  self->result = self->vector_call(self->callback, self->items, 1, self->kwarg_names);
-  self->items[0] = Py_None;
+    EagerRoutineObject *const self = (EagerRoutineObject *)self_obj;
+    self->items[0] = args[0];
+    self->result = self->vector_call(self->callback, self->items, 1, self->kwarg_names);
+    self->items[0] = Py_None;
 
-  return Py_NewRef(self_obj);
+    return Py_NewRef(self_obj);
 }
 
 static PySendResult EagerRoutine_am_send(PyObject *const self_obj, PyObject *const arg, PyObject **presult) {
-  EagerRoutineObject *const self = (EagerRoutineObject *)self_obj;
-  *presult = self->result;
-  self->result = Py_None;
+    EagerRoutineObject *const self = (EagerRoutineObject *)self_obj;
+    *presult = self->result;
+    self->result = Py_None;
 
-  return PYGEN_RETURN;
+    return PYGEN_RETURN;
 }
 
 static PyObject *EagerRoutine_new(PyTypeObject *type, PyObject *args, PyObject *kwargs) {
-  const Py_ssize_t kwargs_count = kwargs ? PyDict_GET_SIZE(kwargs) : 0;
-  EagerRoutineObject *self = NULL;
-  PyObject *key = NULL;
-  PyObject *value = NULL;
-  Py_ssize_t position = 0;
+    const Py_ssize_t kwargs_count = kwargs ? PyDict_GET_SIZE(kwargs) : 0;
+    EagerRoutineObject *self = NULL;
+    PyObject *key = NULL;
+    PyObject *value = NULL;
+    Py_ssize_t position = 0;
 
-  if ((self = (EagerRoutineObject *)type->tp_alloc(type, 0)) == NULL) {
-    goto error;
-  }
+    if ((self = (EagerRoutineObject *)type->tp_alloc(type, 0)) == NULL) {
+        goto error;
+    }
 
-  if (PyTuple_GET_SIZE(args) < 1) {
-    PyErr_SetString(PyExc_TypeError, "EagerRoutine requires at least one argument");
-    goto error;
-  }
+    if (PyTuple_GET_SIZE(args) < 1) {
+        PyErr_SetString(PyExc_TypeError, "EagerRoutine requires at least one argument");
+        goto error;
+    }
 
-  if ((self->items = PyMem_Calloc(kwargs_count + 1, sizeof(PyObject *))) == NULL) {
-    PyErr_NoMemory();
-    goto error;
-  }
+    if ((self->items = PyMem_Calloc(kwargs_count + 1, sizeof(PyObject *))) == NULL) {
+        PyErr_NoMemory();
+        goto error;
+    }
 
-  if (kwargs_count > 0 && (self->kwarg_names = PyTuple_New(kwargs_count)) == NULL) {
-    goto error;
-  }
+    if (kwargs_count > 0 && (self->kwarg_names = PyTuple_New(kwargs_count)) == NULL) {
+        goto error;
+    }
 
-  for (Py_ssize_t i = 0; i < kwargs_count && PyDict_Next(kwargs, &position, &key, &value); i++) {
-    PyTuple_SET_ITEM(self->kwarg_names, i, Py_NewRef(key));
-    self->items[1 + i] = Py_NewRef(value);
-  }
+    for (Py_ssize_t i = 0; i < kwargs_count && PyDict_Next(kwargs, &position, &key, &value); i++) {
+        PyTuple_SET_ITEM(self->kwarg_names, i, Py_NewRef(key));
+        self->items[i + 1] = Py_NewRef(value);
+    }
 
-  self->callback = Py_NewRef(PyTuple_GET_ITEM(args, 0));
-  self->vector_call = PyVectorcall_Function(self->callback) ?: PyObject_Vectorcall;
-  self->self_vector_call = EagerRoutine_vectorcall;
-  self->result = Py_None;
-  self->items[0] = Py_None;
+    self->callback = Py_NewRef(PyTuple_GET_ITEM(args, 0));
+    self->vector_call = PyVectorcall_Function(self->callback) ?: PyObject_Vectorcall;
+    self->self_vector_call = EagerRoutine_vectorcall;
+    self->result = Py_None;
+    self->items[0] = Py_None;
 
-  return (PyObject *)self;
+    return (PyObject *)self;
 
 error:
-  Py_XDECREF(self);
-  return NULL;
+    Py_XDECREF(self);
+    return NULL;
 }
 
 static PyAsyncMethods EagerRoutine_async = {
@@ -119,37 +119,37 @@ static PyTypeObject EagerRoutine_Type = {
 };
 
 PyMODINIT_FUNC PyInit_routine(void) {
-  PyObject *coroutine_abc = NULL;
-  PyObject *register_abc = NULL;
-  PyObject *module = NULL;
+    PyObject *coroutine_abc = NULL;
+    PyObject *register_abc = NULL;
+    PyObject *module = NULL;
 
-  if (PyType_Ready(&EagerRoutine_Type) < 0) {
-    goto error;
-  }
+    if (PyType_Ready(&EagerRoutine_Type) < 0) {
+        goto error;
+    }
 
-  if ((coroutine_abc = PyImport_ImportModuleAttrString("collections.abc", "Coroutine")) == NULL) {
-    goto error;
-  }
+    if ((coroutine_abc = PyImport_ImportModuleAttrString("collections.abc", "Coroutine")) == NULL) {
+        goto error;
+    }
 
-  if ((register_abc = PyObject_CallMethod(coroutine_abc, "register", "O", (PyObject *)&EagerRoutine_Type)) == NULL) {
-    goto error;
-  }
+    if ((register_abc = PyObject_CallMethod(coroutine_abc, "register", "O", (PyObject *)&EagerRoutine_Type)) == NULL) {
+        goto error;
+    }
 
-  if ((module = PyModule_Create(&module_definition)) == NULL) {
-    goto error;
-  }
+    if ((module = PyModule_Create(&module_definition)) == NULL) {
+        goto error;
+    }
 
-  if (PyModule_AddObjectRef(module, "EagerRoutine", (PyObject *)&EagerRoutine_Type) < 0) {
-    goto error;
-  }
+    if (PyModule_AddObjectRef(module, "EagerRoutine", (PyObject *)&EagerRoutine_Type) < 0) {
+        goto error;
+    }
 
-  Py_DECREF(register_abc);
-  Py_DECREF(coroutine_abc);
-  return module;
+    Py_DECREF(register_abc);
+    Py_DECREF(coroutine_abc);
+    return module;
 
 error:
-  Py_XDECREF(module);
-  Py_XDECREF(register_abc);
-  Py_XDECREF(coroutine_abc);
-  return NULL;
+    Py_XDECREF(module);
+    Py_XDECREF(register_abc);
+    Py_XDECREF(coroutine_abc);
+    return NULL;
 }
