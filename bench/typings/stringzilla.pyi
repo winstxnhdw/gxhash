6cad6d652e391d22c22ec7acb917150c3ee0614b
winstxnhdw <winstxnhdw@gmail.com>
2026-03-23 02:21:08 +0800

    revert(refactor): rewrite in rust
    This reverts commit d7d7c37ff18ff7f1bd1b947e5bfc6c55867eb689.

--- bench/typings/stringzilla.pyi
+++ bench/typings/stringzilla.pyi
@@ -1,5 +1,3 @@
 from collections.abc import Buffer
 
-from cityhash import Uint64
-
-def hash(data: str | Buffer, /, seed: int = ...) -> Uint64: ...  # noqa: A001
+def hash(data: str | Buffer, /, seed: int = ...) -> int: ...  # noqa: A001
